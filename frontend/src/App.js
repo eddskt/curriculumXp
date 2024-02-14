c8f8d47ff9a499561dcd28b2fc883682a1558f06
Edson Filho <edsonfilho10@gmail.com>
2024-02-14 00:49:12 +0000

    Experience Window Basics Finalized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,32 +8,35 @@ import { Desktop } from './pages/desktop';
 import { Bios } from './pages/bios';
 import { isMobile, isTablet, isDesktop } from 'react-device-detect';
 import DeathScreenWithoutReturn from './pages/components/deathScreen/deathScreenWithoutReturn';
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
+  console.log("Are you looking for an error? You will find it, because I'm not perfect... \n \n If you are looking for security flaws, you will find them, as I am not an information security expert. \n \n Now, if you liked the project and are trying to see the code, it is available at https:gitbub.com/eddskt/curriculumXp")
+  const helmetContext = {};
   const routes = useRoutes([
     {
       path: "/",
-      element: <PrivateRoutes><Desktop /></PrivateRoutes>,
+      element: <HelmetProvider context={helmetContext}><PrivateRoutes><Desktop /></PrivateRoutes></HelmetProvider>,
       children: [
         
       ]
     },
     {
       path: "/login",
-      element: <Login />,
+      element: <HelmetProvider context={helmetContext}><Login /></HelmetProvider>,
       index: true,
     },
     {
       path: "/register",
-      element: <Register />,
+      element: <HelmetProvider context={helmetContext}><Register /></HelmetProvider>,
     },
     {
       path: "/forgotpassword",
-      element: <ForgotPassword />,
+      element: <HelmetProvider context={helmetContext}><ForgotPassword /></HelmetProvider>,
     }
   ]);
 
-  return isDesktop ? routes : <DeathScreenWithoutReturn error={'DEVICE_IS_NOT_VALID'} steps={'Access from another Device'}/>;
+  return isDesktop ? routes : <HelmetProvider context={helmetContext}> <DeathScreenWithoutReturn error={'DEVICE_IS_NOT_VALID'} steps={'Access from another Device'}/> </HelmetProvider>;
 
   // return (
   //   <div className='App'>
